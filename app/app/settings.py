edf591266fe69a3e64999fdc83089097c1c68173
DiegoZamora33 <diego.zamora33.dz@gmail.com>
2020-11-10 23:06:41 -0600

    Pull Zamora

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': 'localhost',
+        'NAME': 'aulas',
+        'USER': 'postgres',
+        'PASSWORD': '2694tekumacC',
     }
 }
 
