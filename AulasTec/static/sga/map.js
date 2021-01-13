7a4fae63483b0db7739ca1ebd345743d869f4bc2
squatcub <brandon.alexis10@hotmail.com>
2021-01-13 17:58:25 -0600

    Cambios para layouts y modals

--- AulasTec/static/sga/map.js
+++ AulasTec/static/sga/map.js
@@ -6,8 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
 function listener() {
     document.querySelectorAll('.listen').forEach(btn => {
         btn.onclick = function () {
-            aula = btn.id;
-            alert(aula);
+            var modal = document.getElementById("myModal");
+            modal.innerHTML = `<!-- Modal content -->
+            <div class="modal-content-map">
+              <span id="close" class="close">&times;</span>
+              <p>Seleccionaste el aula ${btn.id}</p>
+            </div>`;
+            var span = document.getElementById("close");
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            setTimeout(() => {  modal.style.display = "block"; }, 500);
         }
     });
 };
