24d84a8584601a5ea5f137d7ec7acf162c34ff11
DiegoZamora33 <diego.zamora33.dz@gmail.com>
2021-01-12 20:00:50 -0600

    Data Base, Modelos Creados

--- AulasTec/models.py
+++ AulasTec/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+# Model User Custom
+class User(AbstractUser):
+    pass
+    celphone = models.CharField(max_length=12, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Edificio(models.Model):
+	nombre = models.CharField(max_length=5, null=False)
+	especialidad = models.CharField(max_length=100, null=True)
+	descripcion = models.CharField(max_length=200, null=True)
+	numAulas = models.IntegerField()
+	numPisos = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+class Aula(models.Model):
+	edificio = models.ForeignKey(Edificio, on_delete=models.CASCADE, related_name="Aula_Edificio", null=False)
+	nombre = models.CharField(max_length=5, null=False)
+	descripcion = models.CharField(max_length=200, null=True)
+	horario = models.FileField(upload_to="MEDIA/Horarios/", null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+class Pregunta(models.Model):
+	pregunta = models.CharField(max_length=500, null=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+class Respuesta(models.Model):
+	respuesta = models.CharField(max_length=500, null=False)
+	valor = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+class Encuesta(models.Model):
+	pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE, related_name="Pregunta_Encuesta", null=False)
+	respuesta = models.ForeignKey(Respuesta, on_delete=models.CASCADE, related_name="Respuesta_Encuesta", null=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	
